778e3a78fd21936edeae3499a3802c63d5d35ef6
sceonn <yoou201@naver.com>
2022-10-25 22:44:10 +0900

    lecture: MVC와 템플릿 엔진

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -3,12 +3,19 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
     @GetMapping("hello")
     public  String Hello(Model model) {
-        model.addAttribute("data", "hello!");
+        model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String HelloMvc(@RequestParam("name") int name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
