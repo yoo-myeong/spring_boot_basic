ad4fe216f1a05133c911b65e2c9bce0fff8b9773
sceonn <yoou201@naver.com>
2022-10-27 23:13:33 +0900

    lecture: 회원웹기능-홈화면추가

---
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
