96ce432be9deaf4e9db3e42c8670a7ea6045dbc3
sceonn <yoou201@naver.com>
2022-10-29 10:21:31 +0900

    lecture: 회원 웹 기능-등록, 조회

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
 }
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -38,7 +38,7 @@ public class MemberService {
     /**
      * 전체 회원 조회
      */
-    public List<Member> findMemebers() {
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
